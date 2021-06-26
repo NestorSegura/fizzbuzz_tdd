3727cd37663aaf392a92fdb31ef9a19fe61cf3ca
Nestor Segura <nestor.segura@gmail.com>
2021-06-26 14:55:19 +0200

    first tests cycle

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,22 +1,7 @@
 
 const fizzBuzz = (number) => {
 
-    // todo número divisible entre 3 y 5 fizzBuzz
-    if (number % 15 === 0) {
-        return 'fizzBuzz'
-    }
-    
-    // todo número divisible entre 5 buzz
-    if (number % 5 === 0) { 
-        return 'buzz'
-    }
-    
-    // todo número divisible entre 3 fizz
-    if (number % 3 === 0) { 
-        return 'fizz'
-    } 
-
-    return number
+    return null
 }
 
 module.exports = {
