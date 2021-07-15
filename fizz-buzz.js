df7c40660b9b8bb8cd7d5774d8a584e4a73bf7c5
Nestor Segura <nestor.segura@gmail.com>
2021-07-15 23:31:51 +0200

    final cycle without implementation

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,10 +1,14 @@
 
 const fizzBuzz = (number) => {
 
+    if (number % 3 === 0 && number % 5 ===0) {
+        return 'fizzBuzz';
+    }
+
     if (number % 5 === 0) {
         return 'buzz'
     }
-    
+
     if (number % 3 === 0) {
         return 'fizz'
     }
