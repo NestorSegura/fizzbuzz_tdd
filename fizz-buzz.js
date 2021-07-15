e41b7d70e9edd64515b603e1289a72e0a62d0ded
Nestor Segura <nestor.segura@gmail.com>
2021-07-15 23:29:58 +0200

    Third cycle without implementation

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,22 +1,14 @@
 
 const fizzBuzz = (number) => {
 
-    // todo número divisible entre 3 y 5 fizzBuzz
-    if (number % 15 === 0) {
-        return 'fizzBuzz'
-    }
-    
-    // todo número divisible entre 5 buzz
-    if (number % 5 === 0) { 
+    if (number % 5 === 0) {
         return 'buzz'
     }
     
-    // todo número divisible entre 3 fizz
-    if (number % 3 === 0) { 
+    if (number % 3 === 0) {
         return 'fizz'
-    } 
+    }
 
-    return number
 }
 
 module.exports = {
--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -6,34 +6,21 @@ describe("testing fizzbuzz function", () => {
     test("test buzz", () => {
         const expected = 'buzz'
         expect(fizzBuzz(5)).toBe(expected)
-        expect(fizzBuzz(25)).toBe(expected)
-        expect(fizzBuzz(50)).toBe(expected)
-        expect(fizzBuzz(35)).toBe(expected)
-    
+
     })
 
     test("test fizz", () => {
         const expected = 'fizz'
-
-        expect(fizzBuzz(18)).toBe(expected)
         expect(fizzBuzz(3)).toBe(expected)
-        expect(fizzBuzz(21)).toBe(expected)
     })
 
     test("test fizzBuzz", () => {
         const expected = 'fizzBuzz'
-        expect(fizzBuzz(60)).toBe(expected)
         expect(fizzBuzz(15)).toBe(expected)
-        expect(fizzBuzz(30)).toBe(expected)
-        expect(fizzBuzz(3000)).toBe(expected)
-        expect(fizzBuzz(75)).toBe(expected)
-        
     })
 
     test("test output number",  () => {
-        expect(fizzBuzz(7)).toBe(7);
-        expect(fizzBuzz(11)).toBe(11);
-        expect(fizzBuzz(14)).toBe(14);
+        expect(fizzBuzz(1)).toBe(1);
     })
 
 })
